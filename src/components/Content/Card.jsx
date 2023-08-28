5fa8b7b906d5df5085ed920f3f88373b598ecd90
AnujDagade <dagadeanuj@gmail.com>
2023-08-28 12:15:02 +0000

    Cards done

---
+++ src/components/Content/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Card.css'
+
+export default function Card(props) {
+    return (
+        <div className='card'>
+            <div className='name'>
+                <h4>{props.name}</h4>
+            </div>
+            <div>
+                <p>
+                    {props.desc}
+                </p>
+            </div>
+            <div>
+                {props.contact}
+            </div>
+        </div>
+    )
+}
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import TopBar from './TopBar'
+import Card from './Card'
+import Data from '../../../Data'
+import './Content.css'
+
+const cards = Data.map(
+  (card) => (<Card name={card.name}
+    desc={card.desc}
+    contact={card.contact} />)
+)
+
+
+
 
 export default function Content() {
+
+
   return (
     <div className='content'>
-        <TopBar />
-        Demo
+      <TopBar />
+      <div className="cards">
+        {cards}
+      </div>
     </div>
   )
 }
