c0e142a569d2747a0bfbc213bc491db1afdd71e5
AnujDagade <dagadeanuj@gmail.com>
2023-09-21 14:26:11 +0000

    Linked in profile opens in separate

--- src/components/Static/Contact.jsx
+++ src/components/Static/Contact.jsx
@@ -4,7 +4,7 @@ import './style.css'
 export default function Contact() {
   return (
     <div className='container'>
-        Linked in: <a href="https://www.linkedin.com/in/anuj-dagade/"> Anuj Dagade</a>
+        Linked in: <a href="https://www.linkedin.com/in/anuj-dagade/" target='_blank'> Anuj Dagade</a>
     </div>
   )
 }
