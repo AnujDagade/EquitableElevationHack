61036c297ea08d556cd3f73bcd0dea2d63f6901e
AnujDagade <dagadeanuj@gmail.com>
2023-10-01 10:49:22 +0000

    .

--- netlify/functions/savengo/savengo.js
+++ netlify/functions/savengo/savengo.js
@@ -4,65 +4,46 @@ import { MongoClient } from "mongodb";
 const handler = async (event) => {
 
   let client = null
-  function initDB() {
-    const URI = process.env.VITE_APP_MONGO_DB_CONNECTIONSTRING
-    
-    client = new MongoClient(
-      URI
-    )
-    let conn = null
+  let result = null
 
-    try {
-      client.connect()
-      conn = client.db("Hope")
-    } catch (error) {
-      console.error(error)
-    }
-    finally {
-      return conn
-    }
-  }
+  const URI = process.env.VITE_APP_MONGO_DB_CONNECTIONSTRING
 
+  client = new MongoClient(
+    URI
+  )
+  let conn = null
 
-
-  async function saveToNgo(ngoInfo) {
-    const conn = initDB()
-    let result = null
-    const ngo = conn.collection("NGO")
-
-    try {
-
-      result = await ngo.insertOne(ngoInfo)
-
-      // for await (const doc of result) {
-      //     console.log(doc);
-      // }
-    } catch (error) {
-      console.log(error)
-    }
-   
-  }
-
-
-
-
-
+  
 
   try {
     const data = JSON.parse(event.body)
-    saveToNgo(data)
-    return {
-      statusCode: 200,
-      body: JSON.stringify({info:data}),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    client.connect()
+    conn = client.db("Hope")
+    const ngo = conn.collection("NGO")
+    result = await ngo.insertOne(ngoInfo)
+    console.warn(`Id ${result}`)
+    // for await (const doc of result) {
+    //     console.log(doc);
+    // }
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  } finally{
-    client.close()
+    console.log(error)
   }
+
 }
+
+try {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ info: result }),
+    // // more keys you can return:
+    // headers: { "headerName": "headerValue", ... },
+    // isBase64Encoded: true,
+  }
+} catch (error) {
+  return { statusCode: 500, body: error.toString() }
+} finally {
+  client.close()
+}
+
 
 export { handler }
